dd077fb7c108fe449703afffa6ca3fbd0be2ddf4
Per Carlsson <perca944@gmail.com>
2012-02-12 16:10:24 +0100

    First commit

---
+++ src/net/bahnhof/poker/PokerApp.java
@@ -0,0 +1,27 @@
+package net.bahnhof.poker;
+
+import android.app.Application;
+
+public class PokerApp extends Application {
+
+	private String currentCard;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+	@Override
+	public void onTerminate() {
+		// not guaranteed to be called
+		super.onTerminate();
+	}
+
+	public String getCurrentCard() {
+		return currentCard;
+	}
+
+	public void setCurrentCard(String currentCard) {
+		this.currentCard = currentCard;
+	}
+}
