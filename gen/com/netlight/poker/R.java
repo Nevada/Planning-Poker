4b4ebeeab71a0c97601c79ddfd3aa11662b0bacc
Per Carlsson <perca944@gmail.com>
2012-03-14 09:23:41 +0100

    Bug fix. Null pointer after restarting the app after a few hours. Also reformating.

--- gen/com/netlight/poker/R.java
+++ gen/com/netlight/poker/R.java
@@ -47,39 +47,37 @@ public final class R {
         public static final int wine=0x7f020019;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f070016;
-        public static final int LinearLayout02=0x7f07000e;
-        public static final int LinearLayout03=0x7f070012;
+        public static final int LinearLayout01=0x7f070015;
+        public static final int LinearLayout02=0x7f07000d;
+        public static final int LinearLayout03=0x7f070011;
         public static final int RelativeLayout01=0x7f070004;
-        public static final int awesomepager=0x7f070009;
-        public static final int b1=0x7f07000b;
-        public static final int b10=0x7f070017;
-        public static final int b11=0x7f070018;
-        public static final int b12=0x7f070019;
-        public static final int b2=0x7f07000c;
-        public static final int b3=0x7f07000d;
-        public static final int b4=0x7f07000f;
-        public static final int b5=0x7f070010;
-        public static final int b6=0x7f070011;
-        public static final int b7=0x7f070013;
-        public static final int b8=0x7f070014;
-        public static final int b9=0x7f070015;
-        public static final int image=0x7f070007;
+        public static final int awesomepager=0x7f070008;
+        public static final int b1=0x7f07000a;
+        public static final int b10=0x7f070016;
+        public static final int b11=0x7f070017;
+        public static final int b12=0x7f070018;
+        public static final int b2=0x7f07000b;
+        public static final int b3=0x7f07000c;
+        public static final int b4=0x7f07000e;
+        public static final int b5=0x7f07000f;
+        public static final int b6=0x7f070010;
+        public static final int b7=0x7f070012;
+        public static final int b8=0x7f070013;
+        public static final int b9=0x7f070014;
         public static final int imageView1=0x7f070003;
-        public static final int linearLayout1=0x7f07000a;
+        public static final int linearLayout1=0x7f070009;
         public static final int number=0x7f070002;
         public static final int scrollView1=0x7f070001;
         public static final int small_number=0x7f070005;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070006;
-        public static final int textView3=0x7f070008;
+        public static final int textView3=0x7f070007;
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int break_card=0x7f030001;
-        public static final int card=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int start_page=0x7f030004;
+        public static final int card=0x7f030001;
+        public static final int pager=0x7f030002;
+        public static final int start_page=0x7f030003;
     }
     public static final class string {
         public static final int about=0x7f050001;
--- src/com/netlight/poker/Card.java
+++ src/com/netlight/poker/Card.java
@@ -1,61 +1,213 @@
 package com.netlight.poker;
 
+import java.util.Random;
+
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 public class Card extends Activity {
 
-	private static final int ABOUT = 0;
-	private static final int SETTINGS = 1;
-	private PokerApp app;
+	private ViewPager awesomePager;
+	private static int NUM_CARDS = 12;
+	private Context context;
+	private AwesomePagerAdapter awesomeAdapter;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.card);
+		setContentView(R.layout.pager);
+		context = this;
 
-		app = (PokerApp) getApplication();
+		awesomeAdapter = new AwesomePagerAdapter();
+		awesomePager = (ViewPager) findViewById(R.id.awesomepager);
+		awesomePager.setAdapter(awesomeAdapter);
 
-		TextView title = (TextView) findViewById(R.id.number);
-		title.setText(app.getCurrentCard());
-		TextView small_title = (TextView) findViewById(R.id.small_number);
-		small_title.setText(app.getCurrentCard());
-		if (app.getCurrentCard().equals("Break?")) {
-			title.setTextSize(70f);
-		}
-	}
+		PokerApp app = (PokerApp) getApplication();
+		awesomePager.setCurrentItem(app.getCurrentIndex());
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add(0, ABOUT, 0, R.string.about);
-		menu.add(0, SETTINGS, 0, R.string.settings);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case ABOUT:
-			Intent about = new Intent(Card.this, About.class);
-			startActivity(about);
-			return true;
-		case SETTINGS:
-			Intent pref = new Intent(Card.this, PreferenceHelper.class);
-			startActivity(pref);
-			return true;
-
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	public void back(View view) {
 		finish();
+	}
+
+	private class AwesomePagerAdapter extends PagerAdapter {
+
+		@Override
+		public int getCount() {
+			return NUM_CARDS;
+		}
+
+		/**
+		 * Create the page for the given position. The adapter is responsible
+		 * for adding the view to the container given here, although it only
+		 * must ensure this is done by the time it returns from
+		 * {@link #finishUpdate()}.
+		 * 
+		 * @param container
+		 *            The containing View in which the page will be shown.
+		 * @param position
+		 *            The page position to be instantiated.
+		 * @return Returns an Object representing the new page. This does not
+		 *         need to be a View, but can be some other container of the
+		 *         page.
+		 */
+		@Override
+		public Object instantiateItem(View collection, int position) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View layout;
+			PokerApp app = (PokerApp) getApplication();
+			String[] texts = app.getCurrentNumbers();
+
+			switch (position) {
+			case 10:
+				layout = inflater.inflate(R.layout.card, null);
+				TextView text = (TextView) layout.findViewById(R.id.number);
+				text.setText("?");
+				TextView small = (TextView) layout
+						.findViewById(R.id.small_number);
+				small.setText("?");
+				break;
+			case 11:
+				layout = inflater.inflate(R.layout.card, null);
+				TextView small2 = (TextView) layout
+						.findViewById(R.id.small_number);
+				small2.setText("Break?");
+
+				setImage(layout);
+				break;
+
+			default:
+				layout = inflater.inflate(R.layout.card, null);
+
+				TextView text2 = (TextView) layout.findViewById(R.id.number);
+				if (texts[position] != null) {
+					text2.setText(texts[position]);
+					TextView small3 = (TextView) layout
+							.findViewById(R.id.small_number);
+					small3.setText(texts[position]);
+				}
+				break;
+			}
+
+			((ViewPager) collection).addView(layout, 0);
+
+			return layout;
+		}
+
+		private void setImage(View layout) {
+			TextView image = (TextView) layout.findViewById(R.id.number);
+
+			Random generator = new Random();
+			int random = generator.nextInt(13);
+			switch (random) {
+			case 0:
+				image.setBackgroundResource(R.drawable.coffee);
+				break;
+			case 1:
+				image.setBackgroundResource(R.drawable.wine);
+				break;
+			case 2:
+				image.setBackgroundResource(R.drawable.drunk);
+				break;
+			case 3:
+				image.setBackgroundResource(R.drawable.tea);
+				break;
+			case 4:
+				image.setBackgroundResource(R.drawable.coffee2);
+				break;
+			case 5:
+				image.setBackgroundResource(R.drawable.milkshake);
+				break;
+			case 6:
+				image.setBackgroundResource(R.drawable.soda);
+				break;
+			case 7:
+				image.setBackgroundResource(R.drawable.bottle);
+				break;
+			case 8:
+				image.setBackgroundResource(R.drawable.can);
+				break;
+			case 9:
+				image.setBackgroundResource(R.drawable.beer);
+				break;
+			case 10:
+				image.setBackgroundResource(R.drawable.glas);
+				break;
+			case 11:
+				image.setBackgroundResource(R.drawable.pot);
+				break;
+			case 12:
+				image.setBackgroundResource(R.drawable.takeaway);
+				break;
+			case 13:
+				image.setBackgroundResource(R.drawable.water);
+				break;
+			default:
+				break;
+			}
+		}
+
+		/**
+		 * Remove a page for the given position. The adapter is responsible for
+		 * removing the view from its container, although it only must ensure
+		 * this is done by the time it returns from {@link #finishUpdate()}.
+		 * 
+		 * @param container
+		 *            The containing View from which the page will be removed.
+		 * @param position
+		 *            The page position to be removed.
+		 * @param object
+		 *            The same object that was returned by
+		 *            {@link #instantiateItem(View, int)}.
+		 */
+		@Override
+		public void destroyItem(View collection, int position, Object view) {
+			((ViewPager) collection).removeView((View) view);
+			// ((ViewPager) collection).removeView((Button) view);
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			// return view == ((Button) object);
+			return view == object;
+		}
+
+		/**
+		 * Called when the a change in the shown pages has been completed. At
+		 * this point you must ensure that all of the pages have actually been
+		 * added or removed from the container as appropriate.
+		 * 
+		 * @param container
+		 *            The containing View which is displaying this adapter's
+		 *            page views.
+		 */
+		@Override
+		public void finishUpdate(View arg0) {
+		}
+
+		@Override
+		public void restoreState(Parcelable arg0, ClassLoader arg1) {
+		}
+
+		@Override
+		public Parcelable saveState() {
+			return null;
+		}
+
+		@Override
+		public void startUpdate(View arg0) {
+		}
+
 	}
 
 }
